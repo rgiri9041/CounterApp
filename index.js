4cae3c3efc230a04a06ab8a9d858214221fbadf4
Raj Kumar Giri <rgiri9041@gmail.com>
2026-05-05 18:49:14 -0500

    showing the previous entries and functionality of save button

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 let countEl = document.getElementById("le-count")
 let incrementBtn = document.getElementById("increment-btn")
+let saveEl = document.getElementById("save-el")
 let saveBtn = document.getElementById("save-btn")
 
 let count = 0
 
 function increment(){
-    count  = count + 1
+    count  += 1
     countEl.innerText = count
 }
 incrementBtn.addEventListener("click", increment)
 
 function save(){
-    saveBtn.innerText = count
- }
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = 0
+
+  }
 saveBtn.addEventListener("click", save)
 
 
